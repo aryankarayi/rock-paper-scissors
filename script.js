c1a286d749f8710ca6b0668b108ce2776962e37d
aryankarayi <aryankarayi@gmail.com>
2023-03-19 11:17:55 -0400

    Add playRound function to script.js

--- script.js
+++ script.js
@@ -1,4 +1,45 @@
 function getComputerChoice() {
-    const choice_list = ["Rock", "Paper", "Scissors"];
+    const choice_list = ["rock", "paper", "scissors"];
     return choice_list[Math.floor(Math.random() * choice_list.length)];
+}
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+
+    if (playerSelection == "rock") {
+        if (computerSelection == "rock") {
+            return "Tie";
+        }
+        else if (computerSelection == "paper") {
+            return "You win! Rock beats Paper"
+        }
+        else if (computerSelection == "scissors") {
+            return "You lose! Scissors beats Rock"
+        }
+    }
+
+    if (playerSelection == "paper") {
+        if (computerSelection == "paper") {
+            return "Tie";
+        }
+        else if (computerSelection == "rock") {
+            return "You win! Paper beats Rock";
+        }
+        else if (computerSelection == "scissors") {
+            return "You lose! Scissors beats Paper";
+        }
+    }
+
+    if (playerSelection == "scissors" || playerSelection == "scissor") {
+        if (computerSelection == "scissors") {
+            return "Tie";
+        }
+        else if (computerSelection == "paper") {
+            return "You win! Scissors beats Paper";
+        }
+        else if (computerSelection == "rock") {
+            return "You lose! Rock beats Scissors";
+        }
+    }
+
 }
